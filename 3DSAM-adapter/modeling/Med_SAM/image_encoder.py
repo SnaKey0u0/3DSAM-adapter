cdccec03612aa967b991c664ce6de0100b5e1088
SnaKey0u0 <48859497+SnaKey0u0@users.noreply.github.com>
2023-09-29 16:49:51 +0800

    show data loader slice, show gpu usage

--- 3DSAM-adapter/modeling/Med_SAM/image_encoder.py
+++ 3DSAM-adapter/modeling/Med_SAM/image_encoder.py
@@ -12,6 +12,19 @@ from segment_anything.modeling.image_encoder import (
     window_unpartition,
 )
 import logging
+from pynvml import *
+
+def print_gpu_utilization():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    print(f"GPU memory occupied: {info.used//1024**2} MB.")
+
+
+def print_summary(result):
+    print(f"Time: {result.metrics['train_runtime']:.2f}")
+    print(f"Samples/second: {result.metrics['train_samples_per_second']:.2f}")
+    print_gpu_utilization()
 
 for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
@@ -367,6 +380,8 @@ class ImageEncoderViT_3d_v2(nn.Module):
                      ### 開始進入block ###
                      """)
         for blk in self.blocks[:6]:
+            print("################")
+            print_gpu_utilization()
             x = blk(x)
             logging.info(f"x經過block: {list(x.size())}")
             idx += 1
